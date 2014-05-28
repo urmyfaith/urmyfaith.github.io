e7ae7777eb434a38293f5a0e3df7321e0ce110c9
cquptzx <cquptzx@qq.com>
2014-05-28 12:19:42 +0800

    config.js to urmyfaith
    hooloo to urmyfaith

--- javascripts/config.js
+++ javascripts/config.js
@@ -1,8 +1,8 @@
-var disqus_shortname = 'hooloodemo';			//Short Name of disqus, this parameter is for disqus comment box.
-var hostbase = 'http://hooloo.github.io';		//The absolute root address of your blog without '/' in the end.
-var githubname = 'hooloo';						//Your username of GitHub.
-var repos = 'hooloo.github.io';					//Name of repository that contains your blog.
-var sitetitle = 'Hooloo';						//Name of your blog.
+var disqus_shortname = 'urmyfaith';			//Short Name of disqus, this parameter is for disqus comment box.
+var hostbase = 'http://urmyfaith.github.io';		//The absolute root address of your blog without '/' in the end.
+var githubname = 'urmyfaith';						//Your username of GitHub.
+var repos = 'urmyfaith.github.io';					//Name of repository that contains your blog.
+var sitetitle = 'urmyfaith';						//Name of your blog.
 var rss = '';									//Rss address of your blog.
 var suffix = '';								//Suffix of post files, '.' should be included.
 var branch = '';								//Branch of your blog repository. 'master' is default if you left it blank.
